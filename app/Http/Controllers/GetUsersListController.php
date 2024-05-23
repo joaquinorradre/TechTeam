bd7e75de1164fc1e76b1526831e7648daaaf9fd8
Helen Portuondo <helenporva@gmail.com>
2024-05-23 10:24:27 +0200

    [2] - Añadido el controlador para el GetUsersList

--- app/Http/Controllers/GetUsersListController.php
+++ app/Http/Controllers/GetUsersListController.php
@@ -1,8 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Services\UserListDataManager;
+use Illuminate\Http\JsonResponse;
 
-class GetUsersListController
+class GetUsersListController extends Controller
 {
+    private UserListDataManager $userListDataManager;
 
+    public function __construct(UserListDataManager $userListDataManager)
+    {
+        $this->userListDataManager = $userListDataManager;
+    }
+
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(): JsonResponse
+    {
+        try {
+            // Obtener los datos de usuarios y streamers
+            $userData = $this->userListDataManager->getUserData();
+
+            return new JsonResponse($userData, 200);
+        }
+        catch (Exception $exception) {
+            return new JsonResponse(['error' => $exception->getMessage()], 500);
+        }
+    }
 }
