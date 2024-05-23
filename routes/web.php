7495e53578e942531980e3c83daf76c445aa552e
Helen Portuondo <helenporva@gmail.com>
2024-05-23 11:21:17 +0200

    [5] - Modificando ruta endpoint5

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,16 @@
 
 use App\Http\Controllers\GetStreamsController;
 use App\Http\Controllers\GetUsersController;
-use App\Http\Controllers\GetTopOfTheTopsController;
-use App\Http\Controllers\PostStreamerController;
-use App\Http\Controllers\DeleteStreamerController;
+use App\Http\Controllers\GetTopOfTheTops;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GetTimelineController;
-use App\Http\Controllers\CreateUserController;
 use App\Http\Controllers\GetUsersListController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/analytics/streams', GetStreamsController::class);
-Route::get('/analytics/streamers', GetUsersController::class);
-Route::get('/analytics/topsofthetops', GetTopOfTheTopsController::class);
-Route::get('/analytics/timeline/:userId', GetTimelineController::class);
-Route::post('/analytics/follow', PostStreamerController::class);
-Route::delete('/analytics/unfollow', DeleteStreamerController::class);
-Route::post('/analytics/users', CreateUserController::class);
+//Route::get('/analytics/users', GetUsersController::class);
+Route::get('/analytics/topsofthetops', [GetTopOfTheTops::class, 'fetchData']);
 Route::get('/analytics/users', GetUsersListController::class);
