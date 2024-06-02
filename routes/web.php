d09e7290b7b06ffdcaf59fc24c5ee415ef91b684
Joaquin Orradre <joaquinorradre@gmail.com>
2024-06-02 17:53:27 +0200

    [73] - Correcion rutas web.php

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([ValidateCsrfToken::class])->group(function () {
-    Route::get('/analytics/formDelete', function () {
-        return view('form-delete');
-    });
-    Route::get('/analytics/formPost', function () {
-        return view('form-post');
-    });
-    Route::get('/analytics/streams', GetStreamsController::class);
-    Route::get('/analytics/streamers', GetUsersController::class);
-    
-});
-
 Route::get('/analytics/streams', GetStreamsController::class);
 Route::get('/analytics/timeline', GetTimelineController::class);
 Route::get('/analytics/streamers', GetUsersController::class);
@@ -35,4 +23,3 @@ Route::post('/analytics/users', CreateUserController::class);
 Route::post('/analytics/follow', PostStreamerController::class);
 Route::delete('/analytics/unfollow', DeleteStreamerController::class);
 
->>>>>>> main
